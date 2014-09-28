c18bd88b4189e73c352c076c9cbf45781d368031
Justin O'Neill <mrrioku@yahoo.com>
2014-09-28 02:15:17 -0600

    added controllers

--- front/js/controllers/homeCtrl.js
+++ front/js/controllers/homeCtrl.js
@@ -1,13 +1,15 @@
 angular
-	.module('app')
-	.controller('homeCtrl', ['$scope', '$timeout', function($scope, $timeout) {
-	    function addSlide() {
-	        var i = target.length;
-	    };
+		.module('app')
+		.controller('homeCtrl', ['$scope', '$timeout', 'Page', function($scope, $timeout, Page) {
+				Page.setTitle('Austin Texas Locksmith');
 
-	    function addSlides() {
-	        for (var i=0; i < qty; i++) {
-	            addSlide(target);
-	        }
-	    }
-	}])
+				function addSlide() {
+						var i = target.length;
+				};
+
+				function addSlides() {
+						for (var i=0; i < qty; i++) {
+								addSlide(target);
+						}
+				}
+		}])
